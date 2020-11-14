56fcc9caf95210129471c3798d45458b3d5f4d6f
Steveen <steveen.echeverri@gmail.com>
2020-11-13 20:39:25 -0500

    First test completed browser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,22 @@ gulp.task('sass', () => {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./public/css'))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('pug', () => {
-    return gulp.src('src/**/*.pug')
+    return gulp.src('./src/pug/*.pug')
         .pipe(pug({ pretty: true }))
         .pipe(gulp.dest('./public'))
+});
+
+gulp.task('default', () => {
+    gulp.watch('src/pug/*.pug', gulp.series('pug'))
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'))
+    gulp.watch('public/**/*.html').on('change', browserSync.reload)
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        }
+    })
 });
